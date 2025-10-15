eb61100e0203ff31b8ef005046a62cdf2ce09b94
EjazRay <raymane@sheridancollege.ca>
2025-10-15 16:50:37 -0400

    Don't need course

--- src/midterm_git_practice/Student.java
+++ src/midterm_git_practice/Student.java
@@ -12,15 +12,6 @@ public class Student {
     private String name; // variables private 
    private int sid; // principle "encapsulation"
 private int sem;
-private String course; 
-
-    public String getCourse() {
-        return course;
-    }
-
-    public void setCourse(String course) {
-        this.course = course;
-    }
     /**
      * @return the name
      */
